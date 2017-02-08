d6400db24706b0b143cbbf702e07bc7d83ac57be
Daniel S. Deboer <daniel.s.deboer@gmail.com>
2017-02-07 20:58:34 -0500

    Fix connection name reverting to default for writes

--- src/TCK/Odbc/OdbcServiceProvider.php
+++ src/TCK/Odbc/OdbcServiceProvider.php
@@ -1,5 +1,6 @@
 <?php namespace TCK\Odbc;
 
+use Illuminate\Database\Connection;
 use Illuminate\Support\ServiceProvider;
 
 class OdbcServiceProvider extends ServiceProvider {
@@ -39,20 +40,17 @@ class OdbcServiceProvider extends ServiceProvider {
 	 */
 	public function boot()
 	{
-		$factory = $this->app['db'];
-		$factory->extend( 'odbc', function ( $config )
-		{
-			if ( ! isset( $config['prefix'] ) )
-			{
-				$config['prefix'] = '';
-			}
+		Connection::resolverFor('odbc', function ($connection, $database, $prefix, $config) {
+            if ( ! isset( $config['prefix'] ) )
+            {
+                $config['prefix'] = '';
+            }
 
-			$connector = new ODBCConnector();
-			$pdo       = $connector->connect( $config );
+            $connector = new ODBCConnector();
+            $pdo       = $connector->connect( $config );
 
-			return new ODBCConnection( $pdo, $config['database'], $config['prefix'] );
-
-		} );
+            return new ODBCConnection($pdo, $database, $config['prefix'], $config);
+        });
 	}
 
 }
